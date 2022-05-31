6bce7d836a861e3c82df8fe1f8061991aa51c749
Plasmaa0 <plasmaa0@gmail.com>
2022-05-31 19:44:56 +0300

    game base class

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-g++ src/main.cpp $(pkg-config --libs --cflags sfml-window sfml-system sfml-graphics) -o main.out
+g++ -I include src/*.cpp $(pkg-config --libs --cflags sfml-window sfml-system sfml-graphics) -o main.out
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
-#include <SFML/Window.hpp>
+#include "Game.hpp"
 
-int main() {
-  sf::Window window(sf::VideoMode(800, 600), "My window");
 
-  // run the program as long as the window is open
-  while (window.isOpen()) {
-    // check all the window's events that were triggered since the last
-    // iteration of the loop
-    sf::Event event;
-    while (window.pollEvent(event)) {
-      // "close requested" event: we close the window
-      if (event.type == sf::Event::Closed) window.close();
-    }
-  }
+void test_application()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "Test application");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+}
 
-  return 0;
+
+int main()
+{
+	//just to check if sfml works
+	// test_application();
+
+	Game g;
+	g.run();
+	return 0;
 }
