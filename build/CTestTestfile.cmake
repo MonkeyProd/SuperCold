734f56b22449dcbeb1fdc265f0d34eb7bc05985b
Viktor Kashirin <36076688+eroticstaff@users.noreply.github.com>
2022-06-08 23:25:16 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/xyamix/Documents/My_Projects/SuperCold
-# Build directory: /Users/xyamix/Documents/My_Projects/SuperCold/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
