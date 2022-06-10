aed67faa586a368a749dff32363aada7f1f017f1
Plasmaa0 <plasmaa0@gmail.com>
2022-06-10 15:43:29 +0300

    line counting script

---
+++ lines.py
@@ -0,0 +1,27 @@
+import fnmatch
+import os
+
+matches = []
+
+def find_files(extension:str):
+	for root, dirnames, filenames in os.walk('src'):
+		for filename in fnmatch.filter(filenames, '*.'+extension):
+			matches.append(os.path.join(root, filename))
+
+extensions=['cpp','hpp','h','c','toml']
+
+for extension in extensions:
+	find_files(extension)
+
+matches = list(filter(lambda x: 'external' not in x, matches))
+# print(matches)
+
+total = 0
+
+for match in matches:
+	with open(match, 'r') as f:
+		l = sum(1 for _ in f)
+		total += l
+		print(l, match)
+
+print(f'\n{total=}')
