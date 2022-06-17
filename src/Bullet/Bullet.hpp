7ed98972b322e3a1ff6e61476519e0d3b728af9d
xyamix <xyamix1337@gmail.com>
2022-06-18 01:31:17 +0300

    Description added #2

--- src/Bullet/Bullet.hpp
+++ src/Bullet/Bullet.hpp
@@ -3,17 +3,38 @@
 #include <SFML/Graphics.hpp>
 
 class Bullet : public sf::Drawable, public sf::Transformable {
-  GameObject m_bulletObject;
-  sf::Vector2f m_velocity;
+	GameObject m_bulletObject;
+	sf::Vector2f m_velocity;
 
- public:
-  bool isExist = true;
-  Bullet(sf::Vector2f startPosition, sf::Vector2f velocity,
-         std::vector<sf::Sprite> bulletSprite);
+  public:
+	bool isExist = true;
 
-  GameObject &getBulletObject();
-  void moveBullet(sf::Time deltaTime);
-  virtual void draw(sf::RenderTarget &surface, sf::RenderStates states) const;
+	/**
+	 * @brief Класс, пули
+	 *
+	 * @param startPosition начальное положение
+	 * @param velocity вектор скорости
+	 * @param bulletSprite спрайты пули
+	 */
+	Bullet(sf::Vector2f startPosition, sf::Vector2f velocity,
+	       std::vector<sf::Sprite> bulletSprite);
 
-  sf::FloatRect getBulletRect();
+	/**
+	 * @brief Метод, возращающий GameObject пули
+	 */
+	GameObject &getBulletObject();
+
+	/**
+	 * @brief Метод, передвигающий пулю
+	 *
+	 * @param deltaTime deltaTime
+	 */
+	void moveBullet(sf::Time deltaTime);
+
+	virtual void draw(sf::RenderTarget &surface, sf::RenderStates states) const;
+
+	/**
+	 * @brief Метод, возращающий FloatRect пули
+	 */
+	sf::FloatRect getBulletRect();
 };
